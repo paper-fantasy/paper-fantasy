54c12d45a11d6cae054221c5773a6421c8f41fea
djheyson oliveira <djheyson.oliveira@gmail.com>
2021-10-17 10:15:34 -0300

    chore: remove clone of the admin

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -4,4 +4,3 @@
 
 git clone git@github.com:paper-fantasy/paper-fantasy-api.git
 git clone git@github.com:paper-fantasy/paper-fantasy-front.git
-git clone git@github.com:paper-fantasy/paper-fantasy-admin.git
